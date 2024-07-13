8eaff85b001f83531b9cc73bf13402c038d3c467
Hleb <gleb.qb@gmail.com>
2024-07-13 17:30:24 +0200

    spring-boot-1 working

---
+++ spring-boot-1/src/main/java/ru/bublinoid/springboot1/controller/ProductController.java
@@ -0,0 +1,32 @@
+package ru.bublinoid.springboot1.controller;
+
+import ru.bublinoid.springboot1.model.Product;
+import ru.bublinoid.springboot1.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public void createProduct(@RequestBody Product product) {
+        productService.createProduct(product);
+    }
+}
